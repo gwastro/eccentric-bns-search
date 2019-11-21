1e27cdae38d1a097c75a7dbb467eda3e76392104
Alexander Harvey Nitz <alex.nitz@gmail.com>
2019-11-21 21:34:47 +0100

    bank generation scripts

---
+++ banks/ecc.sh
@@ -0,0 +1,18 @@
+OMP_NUM_THREADS=1 python -m cProfile -o log `which pycbc_brute_bank` \
+--verbose \
+--output-file eccbank.hdf \
+--minimal-match 0.965 \
+--tolerance .001 \
+--buffer-length 4 \
+--sample-rate 2048 \
+--tau0-threshold 0.5 \
+--approximant EccentricFD \
+--tau0-crawl 10 \
+--tau0-start 250 \
+--tau0-end 520 \
+--psd-file H1L1-O1_C02_HARM_MEAN_PSD-1126051217-11203200.txt \
+--params mass1 mass2 eccentricity long_asc_nodes inclination \
+--min 1.1 1.1 0 0 -3.14 \
+--max 1.6 1.6 0.2 6.283 3.14 \
+--seed 1 \
+--low-frequency-cutoff 30.0
